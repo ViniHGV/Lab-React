fdd48a4232f48b60e4c065583c5df9ba1014dcff
Vini <ixxvinicius@gmail.com>
2023-01-20 15:06:03 -0300

    Importando a função Lorem

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import HelloWorld from './HelloWorld';
+import Lorem from './components/LoremIpsum';
 
 /*function HelloWorld(){
   return <h1>Hello World !</h1>
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
         <HelloWorld/>
+        <Lorem/>
     </div>
   );
 }
