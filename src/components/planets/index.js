c7d2c73dfc2a233c1f1542bb69494687696506b5
Vini <ixxvinicius@gmail.com>
2023-01-23 16:00:34 -0300

    Criando a função Grayimg

--- src/components/planets/index.js
+++ src/components/planets/index.js
@@ -1,4 +1,5 @@
 import React, {Fragment} from "react";
+import GraImg from "../../shared/gray_img";
 
 const Planets = () => {
     return (
@@ -6,7 +7,7 @@ const Planets = () => {
             <hr/>
             <h4>Mercúrio</h4>
             <p>Mercúrio é o menor planeta do Sistema Solar e o mais próximo do Sol. Sua órbita ao redor do Sol leva 87,97 dias terrestres, o mais curto de todos os planetas do Sol. É nomeado após o deus romano Mercurius, deus do comércio, mensageiro dos deuses e mediador entre deuses e mortais, correspondendo ao deus grego Hermes.</p>
-            <img src="http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcTFFRKg1XEC7kNu58XVZi9vPh6F9ii0FhevCdEAUKyMEtSdx4HLLLNBMacXXX0Y0gLv" className="Img"></img>
+           <GraImg/>
         </div>
     )
 }
---
+++ src/shared/gray_img/index.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+import Planets from "./index";
+
+const GraImg = () => {
+    return (
+        <img src="http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcTFFRKg1XEC7kNu58XVZi9vPh6F9ii0FhevCdEAUKyMEtSdx4HLLLNBMacXXX0Y0gLv" className="Img"></img>
+    )
+}
+
+export default GraImg;
