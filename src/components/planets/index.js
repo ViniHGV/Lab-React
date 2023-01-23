ab7b35eef7c289f71746ae1e8757847282cfaf3c
Vini <ixxvinicius@gmail.com>
2023-01-23 15:31:48 -0300

    Adicionando o titulo e a descirção do planeta

--- src/components/planets/index.js
+++ src/components/planets/index.js
@@ -2,9 +2,11 @@ import React, {Fragment} from "react";
 
 const Planets = () => {
     return (
-        <Fragment>
-            <h3>Planet List</h3>
-        </Fragment>
+        <div>
+            <h3>Lista de planetas</h3>
+            <h4>Mercúrio</h4>
+            <p>Mercúrio é o menor planeta do Sistema Solar e o mais próximo do Sol. Sua órbita ao redor do Sol leva 87,97 dias terrestres, o mais curto de todos os planetas do Sol. É nomeado após o deus romano Mercurius, deus do comércio, mensageiro dos deuses e mediador entre deuses e mortais, correspondendo ao deus grego Hermes.</p>
+        </div>
     )
 }
 
