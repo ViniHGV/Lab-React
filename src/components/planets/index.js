c4271fa6c30da3b1812440eb4129bf8fe63462ac
Vini <ixxvinicius@gmail.com>
2023-01-24 21:36:17 -0300

    adicionando valores de  props Planet em Planets

--- src/components/planets/index.js
+++ src/components/planets/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Planet from "./planet";
 
 const Planets = () => {
     return (
         <>
             <h1>Planet List</h1>
+            <Planet
+            title="Mercurio"
+            description=" é o menor e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar."
+            />
         </>
     )
 }
