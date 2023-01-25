81e6c21cac891e91093a7fefa8c0a3f1a1cedb30
Vini <ixxvinicius@gmail.com>
2023-01-24 21:34:02 -0300

    adicionando  props em Planet

--- src/components/planets/planet/index.js
+++ src/components/planets/planet/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import GrayImg from "../../shared/gray-img";
 
-const Planet = () => {
+const Planet = (props) => {
     return(
         <>
-            <h2>Mercúrio</h2>
-            <p>Mercúrio é o menor planeta do Sistema Solar e o mais próximo do Sol. Sua órbita ao redor do Sol leva 87,97 dias terrestres, o mais curto de todos os planetas do Sol. É nomeado após o deus romano Mercurius, deus do comércio, mensageiro dos deuses e mediador entre deuses e mortais, correspondendo ao deus grego Hermes.</p>
+            <h2>{props.title}</h2>
+            <p>{props.description}</p>
+            
             <GrayImg 
             img_url='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Mercury_in_color_c1000_700_430.png/220px-Mercury_in_color_c1000_700_430.png'
             />
+
         </>
     )
 }
