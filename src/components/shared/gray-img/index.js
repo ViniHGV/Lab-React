71f686094f8209a30c03191d84808d81033884f9
Vini <ixxvinicius@gmail.com>
2023-01-24 21:28:48 -0300

    adicionando props a função GrayImg

--- src/components/shared/gray-img/index.js
+++ src/components/shared/gray-img/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const GrayImg = () => {
+const GrayImg = (props) => {
     return(
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg" className="img"></img>
+        <img src="props.img_url" className="img"></img>
     )
 }
 
