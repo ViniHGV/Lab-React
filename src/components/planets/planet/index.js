0dabfbcd18098a0fce2b8b27d3c87800cd053700
Vini <ixxvinicius@gmail.com>
2023-01-24 21:31:41 -0300

    adicionando props a função GrayImg

--- src/components/planets/planet/index.js
+++ src/components/planets/planet/index.js
@@ -6,7 +6,7 @@ const Planet = () => {
         <>
             <h2>Mercúrio</h2>
             <p>Mercúrio é o menor planeta do Sistema Solar e o mais próximo do Sol. Sua órbita ao redor do Sol leva 87,97 dias terrestres, o mais curto de todos os planetas do Sol. É nomeado após o deus romano Mercurius, deus do comércio, mensageiro dos deuses e mediador entre deuses e mortais, correspondendo ao deus grego Hermes.</p>
-            <GrayImg/>
+            <GrayImg img_url='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Mercury_in_color_c1000_700_430.png/220px-Mercury_in_color_c1000_700_430.png'/>
         </>
     )
 }
--- src/components/shared/gray-img/index.js
+++ src/components/shared/gray-img/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const GrayImg = (props) => {
     return(
-        <img src="props.img_url" className="img"></img>
+        <img src={props.img_url} className="img"></img>
     )
 }
 
